ca2d53f5ecef3b83e5e2be53366cd25fc05bbdc1
codeftsaikat <soikatdey18@gmail.com>
2025-08-26 22:30:14 +0600

    Implement frontend part for the todo application

---
+++ frontend/src/pages/todo/index.tsx
@@ -0,0 +1,15 @@
+import { Card } from "@/components/ui/card"
+
+type Props = {}
+
+const Todo = (props: Props) => {
+    return (
+        <div>
+            <Card className="text-center max-w-md">
+                Here's the data......
+            </Card>
+        </div>
+    )
+}
+
+export default Todo
